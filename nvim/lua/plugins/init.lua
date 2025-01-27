9c26a60f7a3dab91d34fc31d667d900cf8d8855f
Hudson-Liu <hudsonliu0@gmail.com>
2025-01-26 21:02:41 -0500

    fixed rthe build directory issue for latex once and for all

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -32,7 +32,10 @@ return {
       vim.g.tex_flavor = "latex"
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_compiler_latexmk = {build_dir = '/home/j/Documents/Notes/LaTeX_Build/'}
+      vim.g.vimtex_compiler_latexmk = {
+        out_dir = "/home/j/Documents/Notes/LaTeX_Build",
+        options = { "--shell-escape" },
+      }
     end,
   },
 
