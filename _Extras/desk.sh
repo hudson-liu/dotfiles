d16c3af6e66e893e5ae55b23f8e9a67e99410b1a
Hudson Liu <hudsonliu0@gmail.com>
2025-03-02 12:11:03 -0500

    added startup script

---
+++ _Extras/desk.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+
+# -----[Helper Functions]-----
+# Usage: switch_workspace <number>
+switch_workspace() {
+  xdotool key "Super_L+$1"
+  sleep 0.2
+}
+# Usage: launch_background "<command>" <sleep_time>
+launch_background() {
+  eval "$1" &
+  sleep "$2"
+}
+
+# Lower Volume
+pactl set-sink-volume 0 0%
+sleep 1
+
+# -----[Workspace Alpha]-----
+switch_workspace 1
+
+launch_background "google-chrome-stable --profile-directory='Default'" 2
+xdotool key "Super_L+Ctrl+l"
+sleep 0.3
+
+launch_background "kitty" 0.5
+xdotool type "nvim ~/Documents/Notes/Personal/workspaces.md"
+sleep 0.5
+xdotool key Return
+sleep 0.5
+xdotool key "Ctrl+n"
+sleep 0.2
+
+# -----[Workspace Beta]-----
+switch_workspace 2
+
+launch_background "google-chrome-stable --profile-directory='Profile 1'" 2
+
+# -----[Workspace Epsilon]-----
+switch_workspace 5
+
+launch_background "kitty" 0.3
+xdotool key "Super_L+Shift+space"
+sleep 0.3
+bspc node -v -290 0
+sleep 0.1
+bspc node -v 0 230
+sleep 0.1
+bspc node -z left -250 0
+sleep 0.1
+bspc node -z top 0 -420
+sleep 0.1
+xdotool type "clx"
+sleep 0.1
+xdotool key Return
+sleep 0.3
+
+launch_background "kitty" 0.3
+xdotool key "Super_L+Shift+space"
+sleep 0.2
+bspc node -v 290 0
+sleep 0.1
+bspc node -v 0 -200
+sleep 0.1
+bspc node -z top 0 -70
+sleep 0.1
+xdotool type "lowfi"
+sleep 0.1
+xdotool key Return
+sleep 0.2
+
+launch_background "thunar" 1
+xdotool key "Super_L+Shift+space"
+sleep 0.2
+bspc node -v 460 0
+sleep 0.1
+bspc node -v 0 290
+sleep 0.1
+
+# -----[Workspace Zeta]-----
+switch_workspace 6
+launch_background "spotify" 2
+
+# -----[Workspace Eta]---
+switch_workspace 7
+launch_background "vesktop" 2
