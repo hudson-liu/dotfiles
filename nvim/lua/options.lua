b30101c033f67e66c9b36b1c9bdda93518fe6106
Hudson Liu <hudsonliu0@gmail.com>
2025-03-11 22:46:03 -0400

    changes to font and touchpad settings

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,15 +1,10 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
-
 local o = vim.o
+o.cursorlineopt = "both"
 o.relativenumber = true
 o.autochdir = true
 o.expandtab = true
 o.tabstop = 4
 o.softtabstop = 4
 o.shiftwidth = 4
-
